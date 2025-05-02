2e84cfe716878b69ff365f98b8573048dc564d6b
678140 <142547253+678140@users.noreply.github.com>
2025-05-02 18:22:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using BlazorApp1;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer("YourConnectionStringHere"));
+await builder.Build().RunAsync();
